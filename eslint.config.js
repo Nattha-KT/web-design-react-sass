6243044065a7d3fc04d8e25dd791f9e970c5a075
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-11 22:09:13 +0700

    Setup project

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      "out",
+      "build",
+      "public",
+      "*.config.js",
+      "*.setup.js",
+      ".vscode/**",
+      ".prettierrc.js",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          ignoreRestSiblings: false,
+        },
+      ],
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
     },
-  },
-)
+  }
+);
---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+export default {
+  // Check TypeScript files
+  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+
+  // Lint and Format TypeScript and JavaScript
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `npx prettier --write ${filenames.join(' ')} `,
+};
