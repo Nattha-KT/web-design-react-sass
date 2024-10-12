840fdc7defae47c4b110edb3b43d1b78f6218d1f
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-12 12:05:12 +0700

    Fixed config error connot found varibles.scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import './src/styles/_variables.scss';`, // Example of global SCSS import
+        additionalData: `@import './src/styles/utils/_variables';`, // Example of global SCSS import
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
