75f42d7bcd217319cf7aced891ac04dfd50a8354
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-12 09:49:00 +0700

    Start create base structure

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,10 +2,9 @@ export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
   plugins: ['stylelint-scss'],
   rules: {
-    indentation: 2,
-    'color-hex-case': 'lower',
     'max-nesting-depth': 3,
     'no-duplicate-at-import-rules': true,
+    "no-empty-source": null,
   },
   ignoreFiles: ['**/node_modules/**', '**/dist/**'],
 };
