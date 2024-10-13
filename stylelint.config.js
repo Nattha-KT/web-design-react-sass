23625b76f2ec8c6a66da37ede5dfaf252e1c4d2e
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-13 11:39:22 +0700

    Create variance color

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,7 @@ export default {
   rules: {
     'max-nesting-depth': 3,
     'no-duplicate-at-import-rules': true,
+    "scss/no-global-function-names": null,
     "no-empty-source": null,
     'scss/load-no-partial-leading-underscore': null,
     'import-notation': null,
