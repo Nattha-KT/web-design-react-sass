a242d53b67e256a819f06f5826c30170877110ae
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-10-20 23:08:12 +0700

    Prepare to build #font failed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 4096,
+  },
   css: {
     preprocessorOptions: {
       scss: {
